f9fd87af51710a91fffc1f1a96f27859ab95df58
twwct <twwc.tech@gmail.com>
2023-06-07 00:02:19 -0600

    menu class created

---
+++ cli/menu.py
@@ -0,0 +1,16 @@
+
+
+class Menus():
+    def __init__(self, title: str, dash_count: int) -> None:
+        self.title = title
+        self.dash_count = dash_count
+
+    def show_main(self):
+        menu_options: dict = {
+            "1": "Random Password",
+            "2": "Random Passphrase",
+            "3": "Exit"
+        }
+        print(f"{'-'*self.dash_count} {self.title} {'-'*self.dash_count}")
+        for num, options in menu_options.items():
+            print(f"{num} - {options}")
