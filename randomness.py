a7a6fbf4ad5299b492264d8bc071fd226d4c1fc8
twwct <twwc.tech@gmail.com>
2023-06-08 22:49:29 -0600

    general flow created; needs work

--- randomness.py
+++ randomness.py
@@ -1,6 +1,47 @@
+from time import sleep
+from cli.menu import Menus
+from cli.conman import clearcon
+from backend.regiman import RegiMan
+from typing_extensions import LiteralString
+from resources.statics import RegistryVariables as rv
+
 """
-TODO: CREATE THE FLOW
+TODO: COMPLETE THE 'REGIMAN' CLASS BEFORE RUNNING/TESTING MAIN FILE
 """
 
 if __name__ == "__main__":
-    pass
+    # regiman = RegiMan(
+    #     hkeytype=rv.HKEY,
+    #     software_folder=rv.APPNAME
+    # )
+    # regapps = regiman.enum_regapps()
+    # regstatus: bool = regiman.status_check(
+    #     apps=regapps
+    # )
+    # match regstatus:
+    #     case False:
+    #         clearcon()
+    #         apikey_input: LiteralString = input("API key: ").strip()
+    #         regiman.create_key(
+    #             apikey=apikey_input
+    #         )
+    while True:
+        clearcon()
+        Menus(
+            title="MAIN MENU",
+            dash_count=10
+        ).show_main()
+        menu_choice: str = input("Select a menu option: ").strip()
+        match menu_choice:
+            case "1":  # TODO
+                pass
+            case "2":  # TODO
+                pass
+            case "3":
+                clearcon()
+                exit(0)
+            case _:
+                print(
+                    "\nNot a valid option. Please select a menu option [1-3]\n"
+                )
+                sleep(1.5)
