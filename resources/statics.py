c3098184320e09ee94b178b64b4bcd6bd4c11a10
twwct <twwc.tech@gmail.com>
2023-06-11 15:09:24 -0600

    format edit

--- resources/statics.py
+++ resources/statics.py
@@ -13,10 +13,14 @@ class RegistryVariables:
     HKEY: LiteralString = "Software"
     APPNAME: LiteralString = "Randomness"
 
+
 class ConsoleResponses:
     INVALID_INPUT: str = "\nNot a valid option. Please select a menu option [1-3]\n"
+    _CONTINUE = "Press 'Enter' to continue"
+    MENU_OPTIONS = "Select a menu option: "
+
 
 class ExceptionNotes:
-    ENUM_APPS_ERROR = "ERROR: Unable to enumerate apps in the regkey [Line: 22 | File: 'regiman.py']"
-    CREATE_KEY_ERROR = "ERROR: Unable to create the registry key [Line: 44 | File: 'regiman.py']"
-    ENUM_REGKEY_ERROR = "ERROR: Unable to enumerate the regkey [Line: 62 | File: 'regiman.py']"
+    ENUM_APPS_ERROR: str = "ERROR: Unable to enumerate apps in the regkey [Line: 22 | File: 'regiman.py']"
+    CREATE_KEY_ERROR: str = "ERROR: Unable to create the registry key [Line: 44 | File: 'regiman.py']"
+    ENUM_REGKEY_ERROR: str = "ERROR: Unable to enumerate the regkey [Line: 62 | File: 'regiman.py']"
