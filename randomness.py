334bd0bded087ed7f2bf6657bbbef9eeb353bdd1
twwct <twwc.tech@gmail.com>
2023-06-11 17:27:44 -0600

    flow change from line 27-44

--- randomness.py
+++ randomness.py
@@ -2,17 +2,13 @@ from time import sleep
 from cli.menu import Menus
 from cli.conman import clearcon
 from backend.regiman import RegiMan
-from backend.rdoc import RDOProcessor
+from rdoclient import RandomOrgClient
 from typing_extensions import LiteralString
 from resources.statics import RegistryVariables as rv
 from resources.statics import ConsoleResponses as conr
 
-"""
-TODO: FIGURE OUT WHY WHEN CALLING THE API IT RETURNS 'NONE' [BIG PROBLEM] -> LINE: 38 | 'RANDOMNESS.PY' || LINE: 7 | 'RDOC.PY'
-"""
-
 if __name__ == "__main__":
-    regiman = RegiMan(
+    regiman: object = RegiMan(
         hkeytype=rv.HKEY,
         software_folder=rv.APPNAME
     )
@@ -28,6 +24,10 @@ if __name__ == "__main__":
                 apikey=apikey_input
             )
     while regstatus is True:
+        apikey: LiteralString = regiman.enum_key()
+        rd: object = RandomOrgClient(
+            apikey
+        )
         clearcon()
         Menus(
             title="MAIN MENU",
@@ -36,21 +36,17 @@ if __name__ == "__main__":
         menu_choice: str = input(conr.MENU_OPTIONS).strip()
         match menu_choice:
             case "1":
-                # TODO: FIX THIS [NOTHING IS POPULATING WHEN INPUTTING CORRECT REQUESTS - WAS WORKING BEFORE (NOTHING HAS CHANGED)]
-                apikey = regiman.enum_key()
-                print(f"API Key: {apikey}")
-                rdoc = RDOProcessor(
-                    apikey
-                )
-                index_input = input("index: ").strip()
-                minimum_input = input("Min: ").strip()
-                maximum_input = input("Max: ").strip()
-                gen_ints = rdoc.generate_ints(
+                index_input: int = int(input("index: ").strip())
+                index_input = index_input + 2 if index_input == 0 else index_input + \
+                    1 if index_input == 1 else index_input
+                minimum_input: str = input("Min: ").strip()
+                maximum_input: str = input("Max: ").strip()
+                gen_ints: list = rd.generate_integers(
                     index_input,
                     minimum_input,
                     maximum_input
                 )
-                print(gen_ints)
+                print(f"\nResults:\n{gen_ints}\n")
                 input(conr._CONTINUE)
             case "2":  # TODO
                 pass
