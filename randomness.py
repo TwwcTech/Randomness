b9f0ace77122c7f4c696c1ab9576673258ef0021
twwct <twwc.tech@gmail.com>
2023-06-11 15:08:56 -0600

    case 1 complete; errors | needs vetting

--- randomness.py
+++ randomness.py
@@ -2,12 +2,13 @@ from time import sleep
 from cli.menu import Menus
 from cli.conman import clearcon
 from backend.regiman import RegiMan
+from backend.rdoc import RDOProcessor
 from typing_extensions import LiteralString
 from resources.statics import RegistryVariables as rv
 from resources.statics import ConsoleResponses as conr
 
 """
-TODO: CREATE NEW FILE TO PROCESS THE API CALLS [BACKEND]
+TODO: FIGURE OUT WHY WHEN CALLING THE API IT RETURNS 'NONE' [BIG PROBLEM] -> LINE: 38 | 'RANDOMNESS.PY' || LINE: 7 | 'RDOC.PY'
 """
 
 if __name__ == "__main__":
@@ -32,10 +33,25 @@ if __name__ == "__main__":
             title="MAIN MENU",
             dash_count=10
         ).show_main()
-        menu_choice: str = input("Select a menu option: ").strip()
+        menu_choice: str = input(conr.MENU_OPTIONS).strip()
         match menu_choice:
-            case "1":  # TODO
-                pass
+            case "1":
+                # TODO: FIX THIS [NOTHING IS POPULATING WHEN INPUTTING CORRECT REQUESTS - WAS WORKING BEFORE (NOTHING HAS CHANGED)]
+                apikey = regiman.enum_key()
+                print(f"API Key: {apikey}")
+                rdoc = RDOProcessor(
+                    apikey
+                )
+                index_input = input("index: ").strip()
+                minimum_input = input("Min: ").strip()
+                maximum_input = input("Max: ").strip()
+                gen_ints = rdoc.generate_ints(
+                    index_input,
+                    minimum_input,
+                    maximum_input
+                )
+                print(gen_ints)
+                input(conr._CONTINUE)
             case "2":  # TODO
                 pass
             case "3":
