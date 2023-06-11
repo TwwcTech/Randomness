7756c4faf52fc674ca55a9be167042c004d52747
twwct <twwc.tech@gmail.com>
2023-06-11 17:27:55 -0600

    file removed

--- backend/rdoc.py
+++
@@ -1,9 +0,0 @@
-from rdoclient import RandomOrgClient
-
-
-class RDOProcessor(RandomOrgClient):
-    def __init__(self, api_key, blocking_timeout=2.0, http_timeout=10.0, serialized=False):
-        super().__init__(api_key, blocking_timeout, http_timeout, serialized)
-
-    def generate_ints(self, index, minimum, maximum) -> list:
-        self.generate_integers(n=index, min=minimum, max=maximum)
