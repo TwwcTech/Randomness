11359bdd1713a05910c4fce03450f433b3f3ea67
twwct <twwc.tech@gmail.com>
2023-06-11 00:34:34 -0600

    class added, import added

--- resources/statics.py
+++ resources/statics.py
@@ -1,13 +1,17 @@
 import os as ops
 from datetime import datetime
+from typing_extensions import LiteralString
 
 
 class SystemVaraibles:
-    CURRENT_USER = ops.getlogin()
-    DATE_STAMP = datetime.today().date()
-    TIME_STAMP = datetime.today().time()
+    CURRENT_USER: LiteralString = ops.getlogin()
+    DATE_STAMP: LiteralString = datetime.today().date()
+    TIME_STAMP: LiteralString = datetime.today().time()
 
 
 class RegistryVariables:
-    HKEY = "Software"
-    APPNAME = "Randomness"
+    HKEY: LiteralString = "Software"
+    APPNAME: LiteralString = "Randomness"
+
+class ConsoleResponses:
+    INVALID_INPUT: str = "\nNot a valid option. Please select a menu option [1-3]\n"
