87b2b0100f6c85e429f0b65e96563e2706f14141
twwct <twwc.tech@gmail.com>
2023-06-11 19:52:44 -0600

    flow edit

--- randomness.py
+++ randomness.py
@@ -8,12 +8,6 @@ from resources.statics import RDOVariables as rdv
 from resources.statics import RegistryVariables as rv
 from resources.statics import ConsoleResponses as conr
 
-"""
-TODO: STARTING @ LINE 44 [MAIN MENU OPTION 1: GENERATE PASSWORD | 2: GENERATE PIN]
-TODO: STARTING @ LINE 56 [USE THE GENERATE STRING METHOD TO CREATE THE PASSWORD]
-TODO: STARTING @ LING 50 [USE THE GENERATE INTEGER METHOD TO CREATE THE PIN]
-"""
-
 if __name__ == "__main__":
     regiman: object = RegiMan(
         hkeytype=rv.HKEY,
@@ -30,6 +24,7 @@ if __name__ == "__main__":
             regiman.create_key(
                 apikey=apikey_input
             )
+    regstatus: bool = regiman.status_check(regiman.enum_regapps())
     while regstatus is True:
         apikey: LiteralString = regiman.enum_key()
         rd: object = RandomOrgClient(
@@ -47,26 +42,40 @@ if __name__ == "__main__":
                     input("Length of Password [MUST BE AT LEAST 8 CHARACTERS]: ").strip())
                 pass_length = (8 - pass_length) + \
                     pass_length if pass_length < 8 else pass_length
-                print(pass_length)  # TODO
-                gen_string = rd.generate_strings(
+                print(pass_length)
+                gen_pin = rd.generate_strings(
                     n=1,
                     length=pass_length,
                     characters=rdv.PASSWORD_CHAR
                 )
-                print(f"\nResults:\n{gen_string}\n")
+                print(f"\nResults:\n{gen_pin}\n")
                 input(conr._CONTINUE)
             case "2":
-                # index_input: int = int(input("index: ").strip())
-                # index_input = index_input + 2 if index_input == 0 else index_input + \
-                #     1 if index_input == 1 else index_input
-                # minimum_input: str = input("Min: ").strip()
-                # maximum_input: str = input("Max: ").strip()
-                # gen_ints: list = rd.generate_integers(
-                #     index_input,
-                #     minimum_input,
-                #     maximum_input
-                # )
-                # print(f"\nResults:\n{gen_ints}\n")
-                input(conr._CONTINUE)
+                while True:
+                    clearcon()
+                    Menus(
+                        title="PIN GENERATOR",
+                        dash_count=10
+                    ).show_pin_submenu()
+                    pinmenu_input = input("Select a menu option: ")
+                    match pinmenu_input:
+                        case "1":
+                            gen_pin = rd.generate_strings(
+                                n=1,
+                                length=4,
+                                characters=rdv.PIN_CHAR
+                            )
+                            print(f"\nResults:\n{gen_pin}\n")
+                            input(conr._CONTINUE)
+                        case "2":
+                            gen_secure_pin = rd.generate_strings(
+                                n=1,
+                                length=6,
+                                characters=rdv.COMPLEX_PIN_CHAR
+                            )
+                            print(f"\nResults:\n{gen_secure_pin}\n")
+                            input(conr._CONTINUE)
+                        case "3": break
+                        case _: print(conr.INVALID_INPUT), sleep(1.5)
             case "3": clearcon(), exit(0)
             case _: print(conr.INVALID_INPUT), sleep(1.5)
